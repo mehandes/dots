48077c63816af808c61ff9d8884cee53109b8868
Ivan Paraskun <ig.paraskun@gmail.com>
2024-03-26 23:14:30 +0700

    Update.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,7 @@
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
+  use { "catppuccin/nvim", as = "catppuccin" }
+  use 'lervag/vimtex'
   use {
     "ibhagwan/fzf-lua",
     requires = {
@@ -8,11 +10,21 @@ require('packer').startup(function(use)
 }
 end)
 
+require("catppuccin").setup({
+    flavour = "macchiato",
+    transparent_background = true,
+})
+
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+
+vim.cmd.colorscheme "catppuccin"
+
+vim.g.vimtex_view_method = 'zathura'
+vim.g.vimtex_view_general_viewer = 'zathura'
 
 vim.keymap.set("n", "<c-F>", 
   "<cmd>lua require('fzf-lua').git_files()<CR>", { silent = true })
